21238bce18e86c6448e012f689745915050019fc
Moulyagowda-19 <168709974+Moulyagowda-19@users.noreply.github.com>
2025-09-17 21:46:21 +0530

    Add files via upload

---
+++ geatest_common_factor.py
@@ -0,0 +1,13 @@
+def gcdOfStrings(self, str1, str2):
+
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        if str1 + str2 != str2 + str1:
+            return ""
+            
+        gcd_len = gcd(len(str1), len(str2))
+        
+        return str1[:gcd_len]
