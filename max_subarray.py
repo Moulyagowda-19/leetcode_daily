2378fd8581da6c9fcbc841a0ca8d5f033977d76a
Moulyagowda-19 <168709974+Moulyagowda-19@users.noreply.github.com>
2025-09-24 20:46:01 +0530

    max_subarray.py

--- max_subarray.py
+++ max_subarray.py
@@ -3,4 +3,5 @@ def maxSubArray(self,nums):
         for num in nums[1:]:
             current_sum = max(num, current_sum + num)
             max_sum = max(max_sum, current_sum)    
-        return max_sum
+
+        return max_sum  
