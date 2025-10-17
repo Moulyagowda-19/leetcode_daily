419752e83a7b50836376d95a61018ecf8015f850
Moulyagowda-19 <168709974+Moulyagowda-19@users.noreply.github.com>
2025-10-17 21:17:02 +0530

    Format number_of_recentcalls.py for consistency

--- number_of_recentcalls.py
+++ number_of_recentcalls.py
@@ -1,6 +1,5 @@
 from collections import deque
 
-
 def __init__(self):
     self.q = deque()
 
@@ -8,4 +7,5 @@ def ping(self, t):
     self.q.append(t)
     while self.q[0] < t - 3000:
         self.q.popleft()
+
     return len(self.q)
