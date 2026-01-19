58d117906bc3dd4b3a4a6c11a8ba3fdd2613a980
Moulyagowda-19 <168709974+Moulyagowda-19@users.noreply.github.com>
2026-01-19 11:20:34 +0530

    Add files via upload

---
+++ evalaute_division.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Solution:
+    def calcEquation(self, equations, values, queries):
+        graph = defaultdict(list)
+
+        # Build graph
+        for (a, b), val in zip(equations, values):
+            graph[a].append((b, val))
+            graph[b].append((a, 1 / val))
+
+        def dfs(curr, target, visited):
+            if curr == target:
+                return 1.0
+            visited.add(curr)
+
+            for neighbor, weight in graph[curr]:
+                if neighbor not in visited:
+                    result = dfs(neighbor, target, visited)
+                    if result != -1:
+                        return weight * result
+
+            return -1
+
+        results = []
+        for a, b in queries:
+            if a not in graph or b not in graph:
+                results.append(-1.0)
+            else:
+                results.append(dfs(a, b, set()))
+
+        return results
