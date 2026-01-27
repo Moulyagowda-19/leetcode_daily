95bf630cf6dfd6b8441fa7c7ffcef822a550b5ed
Moulyagowda-19 <168709974+Moulyagowda-19@users.noreply.github.com>
2026-01-27 19:45:29 +0530

    Add files via upload

---
+++ guess_number.py
@@ -0,0 +1,16 @@
+def guessNumber(self, n):
+        low = 1
+        high = n
+
+        while low <= high:
+            mid = low + (high - low) // 2
+            result = guess(mid)
+
+            if result == 0:
+                return mid
+            elif result == -1:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return -1
