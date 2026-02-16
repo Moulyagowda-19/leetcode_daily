1b1e2894011cba8e5a3c1ea870693ccbb38d53ff
Moulyagowda-19 <168709974+Moulyagowda-19@users.noreply.github.com>
2026-02-16 19:08:43 +0530

    Implement Trie-based product suggestion system

---
+++ search_system.py
@@ -0,0 +1,34 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.suggestions = []
+
+class Solution:
+    def suggestedProducts(self, products, searchWord):
+        products.sort()
+        
+        root = TrieNode()
+        
+        # Build Trie
+        for product in products:
+            node = root
+            for char in product:
+                if char not in node.children:
+                    node.children[char] = TrieNode()
+                node = node.children[char]
+                if len(node.suggestions) < 3:
+                    node.suggestions.append(product)
+        
+        # Search
+        result = []
+        node = root
+        
+        for char in searchWord:
+            if node and char in node.children:
+                node = node.children[char]
+                result.append(node.suggestions)
+            else:
+                node = None
+                result.append([])
+        
+        return result
